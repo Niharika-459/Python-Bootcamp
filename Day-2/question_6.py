f3cc5c43d11e310f17a2ebc2c763000a50a8ede5
Niharika-459 <128466225+Niharika-459@users.noreply.github.com>
2024-07-20 23:10:13 +0530

    Update question_6.py

--- Day-2/question_6.py
+++ Day-2/question_6.py
@@ -1,11 +1,11 @@
 #take space separated input from user and print alternate elements
 #n=100
-'''li=list(map(int,input().split()))
+li=list(map(int,input().split()))
 #for i in range(0,n,2):
-print(*li[0::2])'''
+print(*li[0::2])
 
 
-'''li=list(map(int,input().split()))
+li=list(map(int,input().split()))
 for i in range(0,len(li),2):
-    print(i,end=" ")'''
+    print(i,end=" ")
 
