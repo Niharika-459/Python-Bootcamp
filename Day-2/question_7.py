ce18843c401aa58f34ea6b876619535df0584491
Niharika-459 <128466225+Niharika-459@users.noreply.github.com>
2024-07-20 23:10:44 +0530

    Update question_7.py

--- Day-2/question_7.py
+++ Day-2/question_7.py
@@ -1,26 +1,26 @@
 #you are given with a comma separated natural numbers 1 to 10.Print only the even numbers
-'''li=list(map(int,input().split(",")))
+li=list(map(int,input().split(",")))
 count=0
 for i in range(1,len(li)+1,1):
     if i%2==0:
         count+=1
         print(i)
     else:
-        count-=1'''
+        count-=1
     
 #----------7.2----------------
-'''print("**count of evens****")
+print("**count of evens****")
 li=list(map(int,input().split(",")))
 count=0
 for i in range(1,len(li)+1,1):
     if i%2==0:
         count+=1
-print(count)'''
+print(count)
 
 #*******7.3********
 # you are given with a space separated integer list find number of even elements and odd 
 # elements in a given list
-'''li=list(map(int,input().split(" ")))
+li=list(map(int,input().split(" ")))
 even=0
 odd=0
 for i in range(len(li)):
@@ -28,7 +28,7 @@ for i in range(len(li)):
         even+=1
     else:
         odd+=1
-print("even count=",even,"\nodd count=",odd)'''
+print("even count=",even,"\nodd count=",odd)
 
 
 
