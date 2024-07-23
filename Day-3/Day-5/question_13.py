1e3e15122f3ed7619dde3cf348123ca7c7ec52be
Niharika-459 <128466225+Niharika-459@users.noreply.github.com>
2024-07-23 23:18:55 +0530

    Add files via upload

---
+++ Day-3/Day-5/question_13.py
@@ -0,0 +1,57 @@
+#check how many vowels are in a string
+'''str=input()
+count=0
+s=str.lower()
+check=['a', 'e','i','o','u']
+for i in (s):
+    if i in(check):
+        count+=1
+print(count)'''
+    
+#write a program to find count of  the consonants
+'''str=input()
+count=0
+s=str.lower()
+check="aeiou"
+for i  in (v):
+     if i not in(check):
+        count+=1
+print(count)'''
+
+#print all the vowels followed by consonants
+'''str=input()
+inp="aeiou"
+res="bcdfghjklmnpqrstvwxyz"
+ans=""
+for i in(str):
+    if i in(res):
+     ans+=i
+print(ans)'''
+
+#print the non-repeating characters/unique characters in a string
+'''str=input()
+res=""
+count=0
+for i in(str):
+   if i not in res:
+      res+=i
+print(res)'''
+
+#print the sum of digits
+'''str=input()
+str2="0123456789"
+count=0
+for i in str:
+        if i in str2:
+         count+=int(i)
+print((count))'''
+
+#reverse the numbers present in the string***
+'''str=input()
+r=int("1234")
+rem=0
+while r!=0:
+    rem=r%10
+
+    r=r//10'''
+
